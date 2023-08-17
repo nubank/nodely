e0a73bd394e944dd551d741054a3e6cc31457e72
joaopluigi <joao.luigi@nubank.com.br>
2023-08-17 15:28:00 -0300

    fix ident

--- test/nodely/engine/core_test.clj
+++ test/nodely/engine/core_test.clj
@@ -19,10 +19,10 @@
 
 (def test-env+cond
   (assoc test-env
-         :z (>cond
-             (>leaf (odd? ?c)) (>leaf [?c ?b])
-             :else :nothing-else-matters)
-         :target (>leaf ?z)))
+    :z (>cond
+        (>leaf (odd? ?c)) (>leaf [?c ?b])
+        :else :nothing-else-matters)
+    :target (>leaf ?z)))
 
 (def simple-env {:x (data/value 2)
                  :y (data/leaf [:x] (fn [{:keys [x]}] (* 2 x)))
@@ -214,17 +214,17 @@
 (defspec all-paths-for-node-doesnt-blow-up-spec
   {:num-tests 30}
   (prop/for-all [env (fixtures/env-gen {:max-branch-count 20})]
-                (core/all-paths-for-node (first (keys env)) env)))
+    (core/all-paths-for-node (first (keys env)) env)))
 
 (defspec all-paths-for-node-dags-without-branches-have-only-one-path-spec
   (prop/for-all [env (fixtures/env-gen {:node-generator fixtures/scalar-gen})]
-                (let [results (map #(core/all-paths-for-node % env) (keys env))]
-                  (every? #(= (count %) 1) results))))
+    (let [results (map #(core/all-paths-for-node % env) (keys env))]
+      (every? #(= (count %) 1) results))))
 
 (defspec commited-dependencies-doesnt-blow-up-spec
   {:num-tests 30}
   (prop/for-all [env (fixtures/env-gen {:max-branch-count 20})]
-                (core/committed-dependencies (first (keys env)) env)))
+    (core/committed-dependencies (first (keys env)) env)))
 
 (defspec commited-dependencies-of-dags-without-branches-are-the-transitive-dependencies-of-target-spec
   (prop/for-all [env (fixtures/env-gen {:node-generator fixtures/scalar-gen})]
