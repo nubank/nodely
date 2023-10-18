2e09851a8bf1eb0dba5e1ab1ef923d34aaa02dc9
Alex Redington <alex.redington@nubank.com.br>
2023-10-18 16:06:55 -0400

    Make all engines run against a closing function

--- src/nodely/engine/manifold.clj
+++ src/nodely/engine/manifold.clj
@@ -20,7 +20,7 @@
 (defn eval-sequence
   [node future-env]
   (let [in-key (::data/input node)
-        f      (::data/fn node)
+        f      (core/sequence-fn prepare-inputs node future-env)
         in     (prepare-inputs [in-key] future-env)]
     (data/value (->> (get in in-key)
                      (mapv #(deferred/future (f %)))
