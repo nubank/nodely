631c1a5b9a593bfe25ffdd943784fcb394e27b85
joaopluigi <joao.luigi@nubank.com.br>
2023-11-14 11:08:25 -0300

    add manifold applicative engine test

--- test/nodely/engine/applicative_test.clj
+++ test/nodely/engine/applicative_test.clj
@@ -29,6 +29,15 @@
                                 :b ?b
                                 :c ?c})})
 
+(def test-env+delay-async {:a (>leaf (+ 1 2))
+                           :b (>leaf (do (Thread/sleep 1000)
+                                         (* ?a 2)))
+                           :c (>leaf (do (Thread/sleep 1000)
+                                         (* ?a 3)))
+                           :d (>leaf {:a ?a
+                                      :b ?b
+                                      :c ?c})})
+
 (def tricky-example {:x (data/value 1)
                      :y (data/value 2)
                      :a (data/value 3)
@@ -206,3 +215,59 @@
                           (s/with-fn-validation
                             (applicative/eval-key env-with-failing-schema :c {::applicative/fvalidate schema/fvalidate
                                                                               ::applicative/context   manifold/context}))))))))
+
+(deftest manifold-eval-key-test
+  (testing "eval promise"
+    (is (match? 3 (applicative/eval-key test-env :c {::applicative/context core-async/context}))))
+  (testing "async works"
+    (let [[time-ns result] (criterium/time-body (applicative/eval-key test-env+delay-async
+                                                                      :d
+                                                                      {::applicative/context manifold/context}))]
+      (is (match? {:a 3 :b 6 :c 9} result))
+      (is (match? (matchers/within-delta 100000000 1000000000) time-ns))))
+  (testing "tricky example"
+    (is (match? 4 (applicative/eval-key tricky-example :z
+                                        {::applicative/context manifold/context})))))
+
+(testing "async works"
+  (let [[time-ns result] (criterium/time-body (applicative/eval-key test-env+delay-async
+                                                                    :d
+                                                                    {::applicative/context manifold/context}))]
+    (is (match? {:a 3 :b 6 :c 9} result))
+    (is (match? (matchers/within-delta 100000000 1000000000) time-ns))))
+
+(deftest manifold-eval-test
+  (testing "eval promise"
+    (is (match? {:a {::data/value 2}
+                 :b {::data/value 1}
+                 :c {::data/value 3}}
+                (applicative/eval test-env :c {::applicative/context manifold/context}))))
+  (testing "tricky example"
+    (is (match? {:x (data/value 1)
+                 :y (data/value 2)
+                 :a (data/value 3)
+                 :b (data/value 4)
+                 :c (data/value 5)
+                 :w (data/value 4)
+                 :z {::data/type :leaf
+                     ::data/inputs #{:w}}}
+                (applicative/eval tricky-example :w {::applicative/context manifold/context})))))
+
+(deftest manifold-eval-env-with-sequence
+  (testing "async response is equal to sync response"
+    (is (match? (-> (core/resolve :b env-with-sequence) (get :b) ::data/value)
+                (applicative/eval-key env-with-sequence :b))))
+  (testing "sync=async for sequence with nil values"
+    (is (match? (-> (core/resolve :b env+sequence-with-nil-values) (get :b) ::data/value)
+                (applicative/eval-key env+sequence-with-nil-values :b))))
+  (testing "sync=async for sequence returning nil values"
+    (is (match? (-> (core/resolve :b env+sequence-returning-nil-values) (get :b) ::data/value)
+                (applicative/eval-key env+sequence-returning-nil-values :b))))
+  (testing "async version takes a third of the time of sync version
+            (runtime diff is 2 sec, within a tolerance of 3ms"
+    (let [[nanosec-sync _]  (criterium/time-body (core/resolve :c env-with-sequence+delay-sync))
+          [nanosec-async _] (criterium/time-body (applicative/eval-key env-with-sequence+delay :c))]
+      (is (match? (matchers/within-delta 8000000 2000000000)
+                  (- nanosec-sync nanosec-async)))))
+  (testing "Actually computes the correct answers"
+    (is (match? [2 3 4] (applicative/eval-key env-with-sequence+delay :c)))))
