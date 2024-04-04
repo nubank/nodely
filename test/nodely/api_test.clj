d2d1dd9087d3e0427480d0efdb7fcad68d0bcd1b
Sophia Velten de Melo <sophia.velten@nubank.com.br>
2024-04-04 16:12:24 -0300

    fix iterative scheduler

--- test/nodely/api_test.clj
+++ test/nodely/api_test.clj
@@ -43,32 +43,3 @@
       (is (thrown-with-msg? clojure.lang.ExceptionInfo  #"Missing key on env"
                             (api/eval-key missing-node-env :c
                                        {::api/engine engine}))))))
-
-#_(deftest nested-cond-macros
-    (testing "using internal variables"
-      (is (= 0
-             (lazy/eval-node-with-values
-              (>cond
-               (>leaf (even? ?z)) (>leaf (+ 2 2))
-               (>leaf (even? ?y)) (>cond
-                                   (>leaf (= 42 ?y)) (>leaf (- 42 ?y))
-                                   (>leaf (odd? ?z)) (>leaf :z-is-odd)))
-              {:z 1, :y 42}))))
-    (testing "using api variables"
-      (is (= 0
-             (nodely/eval-node-with-values
-              (nodely/cond
-                (even? ?z) (+ 2 2)
-                (even? ?y) (>node (nodely/cond
-                                    (= 42 ?y) (- 42 ?y)
-                                    (odd? ?z) :z-is-odd)))
-              {:z 1, :y 42}))))
-    (testing "mixing internal and api variables"
-      (is (= 0
-             (nodely/eval-node-with-values
-              (nodely/cond
-                (even? ?z) (+ 2 2)
-                (even? ?y) (>node (syntax/cond
-                                    (= 42 ?y) (- 42 ?y)
-                                    (odd? ?z) :z-is-odd)))
-              {:z 1, :y 42})))))
