fd2c73953b038ecd213212dc8de3ec0191e644b6
Alex Redington <alex.redington@alex.redington>
2024-05-09 15:26:23 -0400

    Start virtual threads experiment w/ Joao and Sophia

--- src/nodely/engine/virtual_future.clj
+++ src/nodely/engine/virtual_future.clj
@@ -1,14 +1,29 @@
-(ns nodely.engine.virtual-threads
+(ns nodely.engine.virtual-future
   (:import
-   [clojure.lang IBlockingDeref IDeref IPending]
+   [clojure.lang IBlockingDeref IDeref IPending IFn]
    [java.util.concurrent ExecutorService Executors Future]))
 
 (def ^:private virtual-thread-factory
   (-> (Thread/ofVirtual)
-      (.name "magilla-virtual-thread-" 0)
+      (.name "nodely-virtual-thread-" 0)
       .factory))
 
 (def executor-service (Executors/newThreadPerTaskExecutor virtual-thread-factory))
+
+(deftype GreenFuture [^Future executor-future]
+  IDeref
+  (deref [_] (#'clojure.core/deref-future executor-future))
+  IBlockingDeref
+  (deref [_ timeout-ms timeout-val]
+    (#'clojure.core/deref-future executor-future timeout-ms timeout-val))
+  IPending
+  (isRealized [_] (.isDone executor-future))
+  Future
+  (get [_] (.get executor-future))
+  (get [_ timeout unit] (.get executor-future timeout unit))
+  (isCancelled [_] (.isCancelled executor-future))
+  (isDone [_] (.isDone executor-future))
+  (cancel [_ interrupt?] (.cancel executor-future interrupt?)))
 
 (defn future-submit
   "This function is similar to the clojure.core future-call. The only difference is that it receives as a param a dynamic
@@ -16,21 +31,7 @@
   [^ExecutorService executor-service f]
   (let [f (#'clojure.core/binding-conveyor-fn f)
         fut (.submit executor-service ^Callable f)]
-    (reify
-      IDeref
-      (deref [_] (#'clojure.core/deref-future fut))
-      IBlockingDeref
-      (deref
-        [_ timeout-ms timeout-val]
-        (#'clojure.core/deref-future fut timeout-ms timeout-val))
-      IPending
-      (isRealized [_] (.isDone fut))
-      Future
-      (get [_] (.get fut))
-      (get [_ timeout unit] (.get fut timeout unit))
-      (isCancelled [_] (.isCancelled fut))
-      (isDone [_] (.isDone fut))
-      (cancel [_ interrupt?] (.cancel fut interrupt?)))))
+    (GreenFuture. fut)))
 
 (defmacro vfuture
   "Similar to clojure.core future macro. The only difference is that it calls future-submit"
