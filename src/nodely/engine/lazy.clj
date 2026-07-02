b779e878937ee441dff74d88e51e576ccdcdb9f9
joaopluigi <joao.luigi@nubank.com.br>
2026-07-02 15:04:46 -0300

    first protocol for Engine

--- src/nodely/engine/lazy.clj
+++ src/nodely/engine/lazy.clj
@@ -3,7 +3,19 @@
   (:require
    [clojure.core.async :as async]
    [nodely.data :as data]
-   [nodely.engine.core :as core]))
+   [nodely.engine.core :as core]
+   [nodely.engine.protocols :as engine.protocols]))
+
+(deftype LazyEngine []
+  engine.protocols/Engine
+  (-eval [_engine env k _opts]
+    (core/resolve k env))
+
+  (-eval-key [engine env k opts]
+    (data/get-value (engine.protocols/-eval engine env k opts) k))
+
+  (-eval-key-channel [engine env k opts]
+    (async/thread (engine.protocols/-eval-key engine env k opts))))
 
 (defn eval
   [env k]
---
+++ src/nodely/engine/protocols.clj
@@ -0,0 +1,24 @@
+(ns nodely.engine.protocols)
+
+(defprotocol Engine
+  (-eval [engine env k opts] "Applies function f to the value(s) inside the context of the functor fv.")
+  (-eval-key [engine env k opts] "Applies function f to the value(s) inside the context of the functor fv.")
+  (-eval-key-channel [engine env k opts] "Applies function f to the value(s) inside the context of the functor fv."))
+
+(defn eval
+  ([engine env k]
+   (-eval engine env k {}))
+  ([engine env k opts]
+   (-eval engine env k opts)))
+
+(defn eval-key
+  ([engine env k]
+   (-eval-key engine env k {}))
+  ([engine env k opts]
+   (-eval-key engine env k opts)))
+
+(defn eval-key-channel
+  ([engine env k]
+   (-eval-key-channel engine env k {}))
+  ([engine env k opts]
+   (-eval-key-channel engine env k opts)))
